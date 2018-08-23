3bfb11b6995526b2d048fa9e0c216a8d876cfa58
sopr025 <sopr025@fiws227.cs.tu-dortmund.de>
2018-08-23 11:42:39 +0200

    New getUninvitedParticipants diagram

---
+++ tests/controller/InvitationControllerTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author sopr025
+ *
+ */
+public class InvitationControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#InvitationController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testInvitationController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#getWalkingDinnerController()}.
+	 */
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#getUninvitedParticipants()}.
+	 */
+	@Test
+	public void testGetUninvitedParticipants() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#getEmailList(java.util.List)}.
+	 */
+	@Test
+	public void testGetEmailList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#invite(java.util.List)}.
+	 */
+	@Test
+	public void testInvite() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#uninvite(java.util.List)}.
+	 */
+	@Test
+	public void testUninvite() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#getAdressList(java.util.List)}.
+	 */
+	@Test
+	public void testGetAdressList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.InvitationController#exportPDF(java.lang.String)}.
+	 */
+	@Test
+	public void testExportPDF() {
+		fail("Not yet implemented");
+	}
+
+}
