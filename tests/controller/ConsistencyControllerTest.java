3f2d1646ba58c8e300c36e41efa375ab9705b27a
sopr024 <sopr024@fiws226.cs.tu-dortmund.de>
2018-08-23 11:07:21 +0200

    Testklassen erstellt

---
+++ tests/controller/ConsistencyControllerTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author sopr024
+ *
+ */
+public class ConsistencyControllerTest {
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getWalkingDinnerController()}.
+	 */
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#setWalkingDinnerController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getWarnings(model.Team)}.
+	 */
+	@Test
+	public void testGetWarningsTeam() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getInconsistentTeams()}.
+	 */
+	@Test
+	public void testGetInconsistentTeams() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getWarnings(model.Group)}.
+	 */
+	@Test
+	public void testGetWarningsGroup() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getInconsistentGroups()}.
+	 */
+	@Test
+	public void testGetInconsistentGroups() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ConsistencyController#getDifferentRestrictionsFor(java.util.List)}.
+	 */
+	@Test
+	public void testGetDifferentRestrictionsFor() {
+		fail("Not yet implemented");
+	}
+
+}
