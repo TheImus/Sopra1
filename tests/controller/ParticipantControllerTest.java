cfe3d9d1a51efec89b25474598d9de21d62739eb
sopr027 <sopr027@fiws223.cs.tu-dortmund.de>
2018-08-23 11:17:01 +0200

    Added testclasses for ParticipantController and WalkingDinnerController

---
+++ tests/controller/ParticipantControllerTest.java
@@ -0,0 +1,54 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParticipantControllerTest {
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBirthDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWishes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoursePreference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+}
