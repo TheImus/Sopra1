8ead9bdb05402d8a5b873a6b274965955dd7b70c
sopr023 <sopr023@fiws227.cs.tu-dortmund.de>
2018-08-23 12:07:18 +0200

    *Added doc for EventPickerControllerTest *Added doc for EventControllerTest

--- tests/controller/EventControllerTest.java
+++ tests/controller/EventControllerTest.java
@@ -6,51 +6,90 @@ import org.junit.Test;
 
 public class EventControllerTest {
 
+
+	/**
+	 * check if the walkingDinnerController was set
+	 */
 	@Test
 	public void testEventController() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * check if eventName was changed
+	 */
 	@Test
 	public void testSetEventName() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if eventDate was changed
+	 */
 	@Test
 	public void testSetEventDate() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if eventPlace was changed
+	 */
 	@Test
 	public void testSetEventPlace() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if courseTime was changed
+	 */
 	@Test
 	public void testSetCourseTime() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if eventDescription was changed
+	 */
 	@Test
 	public void testSetEventDescription() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if deadline was changed
+	 */
 	@Test
 	public void testSetDeadline() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if the event was deleted and removed from the eventList
+	 */
 	@Test
 	public void testDeleteEvent() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if it returns the walkingDinnerController and not sth else (should work after the Constructor was called) or null
+	 */
 	@Test
 	public void testGetWalkingDinnerController() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if the walkingDinnerController was changed
+	 */
 	@Test
 	public void testSetWalkingDinnerController() {
 		fail("Not yet implemented");
--- tests/controller/EventPickerControllerTest.java
+++ tests/controller/EventPickerControllerTest.java
@@ -6,36 +6,58 @@ import org.junit.Test;
 
 public class EventPickerControllerTest {
 
+	/**
+	 * check if the walkingDinnerController was set
+	 */
 	@Test
 	public void testEventPickerController() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * check if a list was returned and if it is filled with the events
+	 */
 	@Test
 	public void testGetAllEvents() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * enter two lists with disjunct names, search with one of their names and check if the result matches the one 
+	 */
 	@Test
 	public void testSearchEventName() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * check if the new event was added to the list
+	 */
 	@Test
 	public void testNewEvent() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * check if currentEvent was changed of the walkingDinnerController
+	 */
 	@Test
 	public void testModifyEvent() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * check if it returns the walkingDinnerController and not sth else (should work after the Constructor was called) or null
+	 */
 	@Test
 	public void testGetWalkingDinnerController() {
 		fail("Not yet implemented");
 	}
 
+
+	/**
+	 * check if the walkingDinnerController was changed
+	 */
 	@Test
 	public void testSetWalkingDinnerController() {
 		fail("Not yet implemented");
