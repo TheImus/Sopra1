0a595782c47e25792e2e33f70579e7fca5a99153
sopr022 <sopr022@fiws221.cs.tu-dortmund.de>
2018-08-23 11:42:30 +0200

    add JavaDoc for TeamController

--- src/controller/TeamController.java
+++ src/controller/TeamController.java
@@ -4,48 +4,85 @@ import model.Team;
 import model.Participant;
 import java.util.List;
 
+/*
+ * 
+ * @author Julian
+ */
+
 public class TeamController {
 
 	private WalkingDinnerController walkingDinnerController;
 
+	/**this method creates a new Team with only one member. The member is given by the parameter participant. This methods 
+	 * @param participant The only member of the new team, which is created in the method
+	 * @return the new created Team 
+	 */
 	public Team createNewTeam(Participant participant) {
 		return null;
 	}
 
+	/** adds the given participant to the given team. Creates a new team when team is null.
+	 * @param team The team that gets a new person
+	 * @param participant The participant that is added to the given Team
+	 */
 	public void addParticipantToTeam(Team team, Participant participant) {
 
 	}
 
+	/** removes the given participant from the given team.  
+	 * @param team The given participant is deleted from this team
+	 * @param participant The participant that is deleted from the given team
+	 */
 	public void removeParticipantFromTeam(Team team, Participant participant) {
 
 	}
 
-	public List<Participant> getTeamMembers(Team team) {
-		return null;
-	}
 
-	public Participant getHostOfTeam(Team team) {
-		return null;
-	}
-
+	/** creates a list with all teams in the current event
+	 * @return list with all teams in the current event
+	 */
 	public List<Team> getTeams() {
 		return null;
 	}
 
+	/** removes all participants from the given teams and deletes the team afterwards
+	 * @param team The team that is deleted
+	 */
 	public void removeTeam(Team team) {
 
 	}
+	
 
-	public void setTeams(List<Team> teamList) {
-
-	}
-
+	/** getter method for private attribute getWalkingDinnerController
+	 * @return the private attribute getWalkingDinnerController
+	 */
 	public WalkingDinnerController getWalkingDinnerController() {
 		return walkingDinnerController;
 	}
 
+	/** setter method for private attribute getWalkingDinnerController
+	 * @param walkingDinnerController
+	 */
 	public void setWalkingDinnerController(WalkingDinnerController walkingDinnerController) {
 		this.walkingDinnerController = walkingDinnerController;
 	}
+	
+	/** creates a list with all participants in the current event who are not in any team at the moment
+	 * @return the created list with all participants in the current event who are not in any team at the moment
+	 */
+	public List<Participant> getFreeParticipants()
+	{
+		return null;
+	}
+	
+	/** creates a list with all teams in the current event who are not in any group at the moment
+	 * @return the created list with all teams in the current event who are not in any group at the moment
+	 */
+	public List<Team> getFreeTeams()
+	{
+		return null;
+	}
+	
+	
 
 }
