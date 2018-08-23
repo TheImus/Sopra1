c8d878aaf0d1595d6b98c9733638c23edb371cd5
sopr028 <sopr028@fiws222.cs.tu-dortmund.de>
2018-08-23 11:32:54 +0200

    java test case für groupcontroller ergänzt

---
+++ tests/controller/GroupControllerTest.java
@@ -0,0 +1,64 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupControllerTest {
+
+	@Test
+	public void testCreateNewGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddTeamToGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveTeamFromGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGuestTeams() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHostingTeam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGroups() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+}
