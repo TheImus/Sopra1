27855eb86f33a73e128c45b84f825ea018c07e5a
sopr021 <sopr021@fiws225.cs.tu-dortmund.de>
2018-08-23 12:20:08 +0200

    add test for ParticipantActionController

---
+++ tests/controller/ParticipantActionControllerTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author sopr021
+ *
+ */
+public class ParticipantActionControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#getPossibleActions()}.
+	 */
+	@Test
+	public void testGetPossibleActions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#register(model.Participant)}.
+	 */
+	@Test
+	public void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#unregister(model.Participant)}.
+	 */
+	@Test
+	public void testUnregister() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#registerNewPerson()}.
+	 */
+	@Test
+	public void testRegisterNewPerson() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#searchPerson(java.lang.String)}.
+	 */
+	@Test
+	public void testSearchPerson() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#getWalkingDinnerController()}.
+	 */
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantActionController#setWalkingDinnerController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+}
