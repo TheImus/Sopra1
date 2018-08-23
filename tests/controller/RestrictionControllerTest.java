a5ab557d4dc570acddaef4f639f38f029da12cb9
sopr026 <sopr026@fiws219.cs.tu-dortmund.de>
2018-08-23 11:30:25 +0200

    Added Test-Class for RestrictionController

---
+++ tests/controller/RestrictionControllerTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author sopr026
+ *
+ */
+public class RestrictionControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#addNewRestriction(java.lang.String)}.
+	 */
+	@Test
+	public void testAddNewRestriction() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#getEventRestrictions()}.
+	 */
+	@Test
+	public void testGetEventRestrictions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#setParticipantRestrictions(java.util.List)}.
+	 */
+	@Test
+	public void testSetParticipantRestrictions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#getParticipantRestrictions()}.
+	 */
+	@Test
+	public void testGetParticipantRestrictions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#renameRestriction(model.Restriction, java.lang.String)}.
+	 */
+	@Test
+	public void testRenameRestriction() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#getWalkingDinnerController()}.
+	 */
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.RestrictionController#setWalkingDinnerController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+}
