42f59a6f61c220afa774dc9b5c28b4b734fb0ec7
sopr026 <sopr026@fiws219.cs.tu-dortmund.de>
2018-08-23 11:24:08 +0200

    Written JavaDocs for ScheduleController

--- src/controller/ScheduleController.java
+++ src/controller/ScheduleController.java
@@ -9,23 +9,54 @@ import model.Person;
 public class ScheduleController {
 
 	private WalkingDinnerController walkingDinnerController;
+	
+	/**
+	 * Create a new ScheduleController with a reference to
+	 * the central WalkingDinnerController.
+	 * @param walkingDinnerController Central WalkingDinnerController
+	 * @return Returns a ScheduleController-entity with walkingDinnerController-attribute set to param
+	 */
+	public ScheduleController(WalkingDinnerController walkingDinnerController) {
+		this.walkingDinnerController = walkingDinnerController;
+	}
 
+	/**
+	 * DO WE REALLY NEED THIS??!!
+	 * @return Returns a valid Group-Schedule for all Teams
+	 */
 	public List<Group> generateGroups() {
 		return null;
 	}
 
+	/**
+	 * DO WE REALLY NEED THIS?!!!
+	 * @return Returns a valid Team-Schedule for all Participants
+	 */
 	public List<Team> generateTeams() {
 		return null;
 	}
 
+	/**
+	 * Creates a Map for each Participant and the Persons
+	 * he has met in WalkingDinners.
+	 * @return Returns a Map which relates each Participant (as Person) to all Persons he has met in WalkingDinners
+	 */
 	public Map<Person, List<Person>> generateKnowingRelations() {
 		return null;
 	}
 
+	/**
+	 * @return Returns the actually set WalkingDinnerController
+	 */
 	public WalkingDinnerController getWalkingDinnerController() {
 		return walkingDinnerController;
 	}
 
+	/**
+	 * Set the walkingDinnerController-attribute to the given WalkingDinnerController-Entity.
+	 * You should use the parametered constructor to set this attribute when constructing the object.
+	 * @param walkingDinnerController Set the walkingDinnerController-attribute to the given one
+	 */
 	public void setWalkingDinnerController(WalkingDinnerController walkingDinnerController) {
 		this.walkingDinnerController = walkingDinnerController;
 	}
