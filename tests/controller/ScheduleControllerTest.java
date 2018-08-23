fd2261af7a924f7c4901e8af27ed7d582d0901a8
sopr026 <sopr026@fiws219.cs.tu-dortmund.de>
2018-08-23 11:28:20 +0200

    Added a Test-Class for ScheduleController

---
+++ tests/controller/ScheduleControllerTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author sopr026
+ *
+ */
+public class ScheduleControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#ScheduleController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testScheduleController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#generateGroups()}.
+	 */
+	@Test
+	public void testGenerateGroups() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#generateTeams()}.
+	 */
+	@Test
+	public void testGenerateTeams() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#generateKnowingRelations()}.
+	 */
+	@Test
+	public void testGenerateKnowingRelations() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#getWalkingDinnerController()}.
+	 */
+	@Test
+	public void testGetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.ScheduleController#setWalkingDinnerController(controller.WalkingDinnerController)}.
+	 */
+	@Test
+	public void testSetWalkingDinnerController() {
+		fail("Not yet implemented");
+	}
+
+}
