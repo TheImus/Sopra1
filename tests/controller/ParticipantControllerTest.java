e31dcd7fa83671b8a11471546d167ce094a009ce
sopr027 <sopr027@fiws148.cs.tu-dortmund.de>
2018-08-24 13:07:52 +0200

    Added testcases for ParticipantController Test

--- tests/controller/ParticipantControllerTest.java
+++ tests/controller/ParticipantControllerTest.java
@@ -1,67 +1,134 @@
+/**
+ * 
+ */
 package controller;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import model.Event;
+import model.Participant;
+//import model.Person;
+import controller.ParticipantController;
+
 /**
- * @author sopr027 alias Nico
+ * @author sopr027
  *
  */
 public class ParticipantControllerTest {
+	Participant currentParticipant; 
+	ParticipantController participantController; 
+	Event currentEvent;
+	String testName = "H";
+	LocalDate testDate = LocalDate.parse("24.12.2014");
 	
-	
-	public void setup(){
-		
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
 	/**
-	 * Create test object, create a separate object, call the setter methods on it 
-	 * with the attributes which are assigned to the test object and compare them
+	 * @throws java.lang.Exception
 	 */
-	@Test 
-	public void testSetName() {
-		fail("Not yet implemented.");
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		currentEvent = participantController.getWalkingDinnerController().getWalkingDinner().getCurrentEvent();
+		currentParticipant = currentEvent.getCurrentParticipant();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantController#setName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetName() {
+		participantController.setName(testName);		
+		assertEquals(testName, currentParticipant.getPerson().getName());
+
+	}
+
+	/**
+	 * Test method for {@link controller.ParticipantController#setBirthDate(java.time.LocalDate)}.
+	 */
 	@Test
 	public void testSetBirthDate() {
-		fail("Not yet implemented");
+		participantController.setBirthDate(testDate);
+		assertEquals(testDate, currentParticipant.getPerson().getBirthDate());
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setAddress(model.Address)}.
+	 */
 	@Test
 	public void testSetAddress() {
-		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setMail(java.lang.String)}.
+	 */
 	@Test
 	public void testSetMail() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setPhoneNumber(java.lang.String)}.
+	 */
 	@Test
 	public void testSetPhoneNumber() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setWishes(java.lang.String)}.
+	 */
 	@Test
 	public void testSetWishes() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setCoursePreference(model.Course)}.
+	 */
 	@Test
 	public void testSetCoursePreference() {
 		fail("Not yet implemented");
 	}
 
 	/**
-	 * Should return WalkingDinnerController
+	 * Test method for {@link controller.ParticipantController#getWalkingDinnerController()}.
 	 */
 	@Test
 	public void testGetWalkingDinnerController() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link controller.ParticipantController#setWalkingDinnerController(controller.WalkingDinnerController)}.
+	 */
 	@Test
 	public void testSetWalkingDinnerController() {
 		fail("Not yet implemented");
