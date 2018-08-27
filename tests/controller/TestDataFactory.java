6785103b5fd1f406114e01bdeae7cbb7551ea6bf
sopr023 <sopr023@fiws226.cs.tu-dortmund.de>
2018-08-27 14:03:39 +0200

    *Renamed TestDataFactory.createTest to TestDataFactory.createTestWalkingDinnerController

--- tests/controller/TestDataFactory.java
+++ tests/controller/TestDataFactory.java
@@ -86,7 +86,7 @@ public class TestDataFactory {
 		walkingDinner.setCurrentEvent(event2);
 	}
 	
-	private static Event createTestEvent(){
+	public static Event createTestEvent(){
 		
 		return null;
 	}
@@ -94,13 +94,25 @@ public class TestDataFactory {
 	/*
 	 * *Implementation of TestDataFactory
 	 * */
-	public static WalkingDinnerController createTest(){
+	public static WalkingDinnerController createTestWalkingDinnerController(){
 		WalkingDinnerController walkingDinnerController = new WalkingDinnerController();
-		EventPickerController eventPickerController = new EventPickerController(walkingDinnerController);
-		//EventController()
+		WalkingDinnerController wdc = walkingDinnerController;
+		EventPickerController eventPickerController = new EventPickerController(wdc);
+		EventController eventController = new EventController(wdc);
+		InvitationController invitationCotroller = new InvitationController(wdc);
+		RestrictionController restrictionController = new RestrictionController(wdc);
+		ParticipantController participantController = new ParticipantController(wdc);
+		ConsistencyController consistencyController = new ConsistencyController(wdc);
+		ParticipantActionController participantActionController = new ParticipantActionController(wdc);
+		GroupController groupController = new GroupController(wdc);
+		TeamController teamController = new TeamController(wdc);		
 		
+		wdc.setEventPickerController(eventPickerController);
+		wdc.setEventController(eventController);
+		wdc.setInvitationController(invitationCotroller);
+		wdc.setRestrictionController(restrictionController);
+		wdc.setParticipantController(participantController);
 		
-		walkingDinnerController.setEventPickerController(eventPickerController);
 		return walkingDinnerController;
 	}
 
