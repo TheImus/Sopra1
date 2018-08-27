5753b7198645531e5e703908e57b37d0d51e7da2
sopr027 <sopr027@fiws230.cs.tu-dortmund.de>
2018-08-27 14:37:42 +0200

    Added Controller Classes for View

---
+++ src/view/TabInviteController.java
@@ -0,0 +1,16 @@
+package view;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TabInviteController{
+
+    @FXML
+    private Button BtnChangeData;
+
+    @FXML
+    void onModifyEvent(ActionEvent event) {
+
+    }
+
+}
