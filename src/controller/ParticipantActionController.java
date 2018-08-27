df1ca03a0a1261d0c615bdc830942eadd227a155
sopr021 <sopr021@fiws225.cs.tu-dortmund.de>
2018-08-27 13:21:56 +0200

    add implement for participantactioncontroller

--- src/controller/ParticipantActionController.java
+++ src/controller/ParticipantActionController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import model.WalkingDinner;
+import model.Event;
 import model.Participant;
 import model.Person;
 
@@ -9,53 +13,118 @@ public class ParticipantActionController {
 	private WalkingDinnerController walkingDinnerController;
 
 	/**
-	 * This method could return the actions,which are allowed manipulations of the current participant
-	 * the participant in the participantlist of the event could have the actions UNREGISTER, UPDATE_PARTICIPANT;
-	 * the participant in the invitedlist of the event could have the actions REGISTER,UPDATE_PARTICIPANT;
-	 * the participant in the other situation could have the actions UNREGISTER.
+	 * This method could return the actions,which are allowed manipulations of
+	 * the current participant the participant in the participantlist of the
+	 * event could have the actions UNREGISTER, UPDATE_PARTICIPANT; the
+	 * participant in the invitedlist of the event but not in participantlist
+	 * could have the actions REGISTER,UPDATE_PARTICIPANT; the participant in
+	 * the other situation could have the actions REGISTER.
 	 * 
 	 * @return The actions, which could happen in participant operation.
 	 */
 	public List<ParticipantAction> getPossibleActions() {
-		return null;
+		List<ParticipantAction> actions = new ArrayList<>();
+		ParticipantAction register = ParticipantAction.REGISTER;
+		ParticipantAction unregtister = ParticipantAction.UNREGISTER;
+		ParticipantAction update = ParticipantAction.UPDATE_PARTICIPANT;
+		try {
+			Event currentEvent = walkingDinnerController.getWalkingDinner().getCurrentEvent();
+			List<Participant> participantList = currentEvent.getParticipants();
+			List<Participant> invitedList = currentEvent.getInvited();
+			Participant currentParticipant = currentEvent.getCurrentParticipant();
+			if (participantList.contains(currentParticipant)) {
+				actions.add(unregtister);
+				actions.add(update);
+			} else if (invitedList.contains(currentParticipant)) {
+				actions.add(register);
+				actions.add(update);
+			} else {
+				actions.add(register);
+			}
+			return actions;
+		} catch (NullPointerException e) {
+			throw e;
+		}
+
 	}
 
 	/**
-	 * This method add the participant into the participantlist and the invitedlist of the current event.
+	 * This method add the participant into the participantlist and the
+	 * invitedlist of the current event if he or she is not in the invitedlist.
 	 * 
-	 * @param participant is the participant,who is got from database through search. 
+	 * @param participant is the participant,who is got from database through search.
 	 */
 	public void register(Participant participant) {
+		try {
+			Event currentEvent = walkingDinnerController.getWalkingDinner().getCurrentEvent();
+			List<Participant> participantList = currentEvent.getParticipants();
+			List<Participant> invitedList = currentEvent.getInvited();
+			if (!participantList.contains(participant)) {
+				participantList.add(participant);
+			}
+			if (!invitedList.contains(participant)) {
+				invitedList.add(participant);
+			}
+		} catch (NullPointerException e) {
+			throw e;
+		}
 
 	}
 
 	/**
 	 * This method delete the participant from the participantlist of the event.
 	 * 
-	 * @param participant the participant, who wants to quite the current event.
+	 * @param participant is the participant, who wants to quite the current event.
 	 */
 	public void unregister(Participant participant) {
-
+		try {
+			Event currentEvent = walkingDinnerController.getWalkingDinner().getCurrentEvent();
+			List<Participant> participantList = currentEvent.getParticipants();
+			participantList.remove(participant);
+		} catch (NullPointerException e) {
+			throw e;
+		}
 	}
 
 	/**
-	 * This method creates a participant object, which contain the information of the new person,who is not in the database.Then the created participant
-	 * is added in participantlist and invitedlist in current event and return the participant.   
+	 * This method creates a participant object, which contain the information
+	 * of the new person,who is not in the database. Then the created
+	 * participant is added in participantlist and invitedlist in current event and return
+	 * the participant.
 	 * 
-	 * @return  the Participant, who has the information of the new person and is willing to attend the current event.
+	 * @return the Participant, who has the information of the new person and is willing to attend the current event.
 	 */
 	public Participant registerNewPerson() {
-		return null;
+		Person newPerson = new Person();
+		Participant newParticipant = new Participant();
+		newParticipant.setPerson(newPerson);
+		try {
+			Event currentEvent = walkingDinnerController.getWalkingDinner().getCurrentEvent();
+			currentEvent.setCurrentParticipant(newParticipant);
+			currentEvent.getParticipants().add(newParticipant);
+			return newParticipant;
+		} catch (NullPointerException e) {
+			throw e;
+		}
 	}
 
 	/**
 	 * This method return the people in the database, who have the same name as the input.
 	 * 
 	 * @param name is the name of a person.
-	 * @return Personlist ,in the list there are the people, whose name equals to the parameter.  
+	 * @return Personlist ,in the list there are the people, whose name equals
+	 *         to the parameter.
 	 */
 	public List<Person> searchPerson(String name) {
-		return null;
+		try {
+			WalkingDinner walkingDinner = walkingDinnerController.getWalkingDinner();
+			List<Person> allPerson = walkingDinner.getPersons();
+			List<Person> resultOfSearch = allPerson.stream().filter(person -> person.getName().equals(name))
+					.collect(Collectors.toList());
+			return resultOfSearch;
+		} catch (NullPointerException e) {
+			throw e;
+		}
 	}
 
 	public WalkingDinnerController getWalkingDinnerController() {
