1c038a41cbd1c610cddfbde694ba88e9f3f3fe1c
sopr028 <sopr028@fiws231.cs.tu-dortmund.de>
2018-08-29 16:57:53 +0200

    für julian 2

--- src/view/EventOverviewController.java
+++ src/view/EventOverviewController.java
@@ -105,6 +105,8 @@ public class EventOverviewController {
 		for(Event ev:list){
 			listEvent.getItems().add(ev);
 		}
+		BtnEdtiting.setDisable(true);
+    	BtnRemoveEvent.setDisable(true);
 		
     }
     
@@ -123,8 +125,7 @@ public class EventOverviewController {
 		}
     			);
     	refresh();
-    	BtnEdtiting.setDisable(true);
-    	BtnRemoveEvent.setDisable(true);
+    	
 
 
     }
