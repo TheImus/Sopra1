d2762aa0bd7f1c6a0154d508ba1ebc864ffef4b5
sopr028 <sopr028@fiws231.cs.tu-dortmund.de>
2018-08-30 10:12:06 +0200

    btn added

--- src/view/AdjustParticipantController.java
+++ src/view/AdjustParticipantController.java
@@ -97,6 +97,9 @@ import model.Restriction;
 	    private Button BtnNewRestriction;
 	    
 	    @FXML
+	    private Button BtnCallAction;
+	    
+	    @FXML
 	    private GridPane GridPaneAdjustParticipant;
 	    
 	    private WalkingDinnerController walkingDinnerController;
@@ -461,6 +464,11 @@ import model.Restriction;
 	    	Pattern validZipCode = Pattern.compile("\\b\\d{5}\\b");
 	    	
 	    	return validZipCode.matcher(zipCode).matches();
+	    }
+	    
+	    @FXML
+	    void onBtnCallAction(ActionEvent event){
+	    	
 	    }
 	   
 
