11e4c5fc1ab85dbdd6448819a0ee2ff0a8d04081
sopr025 <sopr025@fiws227.cs.tu-dortmund.de>
2018-08-30 15:07:19 +0200

    Get free teams works now

--- src/view/TabGroupsController.java
+++ src/view/TabGroupsController.java
@@ -307,24 +307,18 @@ public class TabGroupsController implements TeamsAUI {
     	refreshSelectedGroupList();
     	refreshFreeTeams();
     	refreshWarnings();
-    	isGeneratable();
+    	refreshGeneratable();
     }
     
-    public void isGeneratable(){
-    	
+    public void refreshGeneratable(){
     	int missingTeams = 3 - walkingDinnerController.getWalkingDinner().getCurrentEvent().getAllTeams().size();
     	
-    	if(missingTeams > 0)
-    	{
-    		
-    		btnGenerateGroups.setDisable(true);
+    	if(missingTeams > 0) {
     		btnGenerateGroups.setText("Generieren (+" + missingTeams +")" );
-    	}
-    	else
-    	{
-    		btnGenerateGroups.setDisable(false);
+    	} else {
     		btnGenerateGroups.setText("Generieren");
     	}
+    	btnGenerateGroups.setDisable(missingTeams > 0);
     }
 
     
