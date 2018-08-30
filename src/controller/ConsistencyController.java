6dbe4086cc86efea05f40ad13a45424dfb5e7502
sopr024 <sopr024@fiws229.cs.tu-dortmund.de>
2018-08-30 15:21:05 +0200

    ConsistencyController warning message etwas anschaulicher gemacht

--- src/controller/ConsistencyController.java
+++ src/controller/ConsistencyController.java
@@ -297,7 +297,7 @@ public class ConsistencyController {// headerCommentRequirement Required
 				for(int j = 0; j < group.getTeams().size();j++){
 					if(groupController.getGroups().get(i).getTeams().contains(group.getTeams().get(j))){
 						String warning = "Team mit Gastgeber " + group.getTeams().get(j).getHost().getPerson().getName() + 
-									"im Gang " +groupController.getCourse() + "kommt in mehreren Gruppen vor. \n";
+									" im Gang " +groupController.getCourse() + " kommt in mehreren Gruppen vor. \n";
 						warning += "Die andere Gruppe besteht aus Gruppe mit Gastgeber: "+groupController.getGroups().get(i).getHostTeam().getHost().getPerson().getName();	
 						warnings.add(warning);	
 					} 
